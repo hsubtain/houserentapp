432c9d93f4a5da7fccfe68aee5ea7c835a97083a
Harris Subtain <harris.subtain@infor.com>
2023-10-02 21:21:04 +0800

    Initial commit

---
+++ src/main/java/com/petproject/houserent/houserentapp/HouserentappApplication.java
@@ -0,0 +1,13 @@
+package com.petproject.houserent.houserentapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouserentappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HouserentappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/petproject/houserent/houserentapp/HouserentappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petproject.houserent.houserentapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouserentappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
